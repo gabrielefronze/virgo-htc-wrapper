39ebd4ac63bce7d4609dbc227c3f489ea29c3656
Gabriele <sucre.91@hotmail.it>
2020-01-09 18:14:04 +0100

    Introducing sub converter

---
+++ sub-converter.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+
+import os.path
+from pathlib import Path
+
+required_input_files = ["./satel_lite", "./proxyrearm", "./plainproxy.pem"]
+
+def getConvertedSubPath(input_sub_file_path : Path):
+    output_sub_file_path_parts = list(input_sub_file_path.parts)
+    output_sub_file_path_parts[-1] = "converted-"+output_sub_file_path_parts[-1]
+    output_sub_file_path = Path('/'.join(output_sub_file_path_parts).replace('//','/'))
+
+    return output_sub_file_path
+
+def purgeLineHeader(line):
+    new_line = line.split('=')[-1]
+    if new_line[0] is ' ':
+        new_line = new_line[1:]
+    return new_line.rstrip("\n\r")
+
+def convertSub(sub_file_path, main_executable_name = None):
+    input_sub_file_path = Path(os.path.abspath(sub_file_path))
+    output_sub_file_path = getConvertedSubPath(input_sub_file_path)
+
+    input_sub = open(input_sub_file_path,"r")
+    output_sub = open(output_sub_file_path, "w+")
+
+    executable_string = ''
+    input_files = ''
+    arguments = ''
+
+    input_files_found = False
+    arguments_found = False
+
+    for line in input_sub:
+        if line.startswith("executable"):
+            executable_string = purgeLineHeader(line)
+            # print("executable: "+executable_string)            
+        elif line.startswith("transfer_input_files"):
+            input_files_found = True
+            input_files = purgeLineHeader(line)
+            # print("input_files: "+input_files)
+        elif line.startswith("arguments"):
+            arguments_found = True
+            arguments = purgeLineHeader(line)
+            # print("arguments: "+arguments)
+
+    new_input_files = input_files+','+','.join(required_input_files)+"\n"
+    main_executable_as_args = "\'\"\""+"./"+executable_string+' '+arguments+"\"\"\'"
+
+    if not main_executable_name:
+        new_arguments = "\""+main_executable_as_args+"\""
+    else:
+        new_arguments = "\""+main_executable_as_args+" --name {}".format(main_executable_name.replace(' ',''))+"\""
+
+    new_arguments = new_arguments+"\n"
+
+    input_sub.seek(0)
+
+    for wline in input_sub:
+        if wline.startswith("executable"):
+            output_sub.write("executable = run-with-proxy-satellite.py\n")
+            # print("executable = run-with-proxy-satellite.py")
+            if not input_files_found:
+                output_sub.write("transfer_input_files = "+new_input_files)
+                # print("transfer_input_files = "+new_input_files)
+            if not arguments_found:
+                output_sub.write("arguments = "+new_arguments)
+                # print("arguments = "+new_arguments)
+        elif wline.startswith("transfer_input_files"):
+            output_sub.write("transfer_input_files = "+new_input_files)
+            # print("transfer_input_files = "+new_input_files)
+        elif wline.startswith("arguments"):
+            output_sub.write("arguments = "+new_arguments)
+            # print("arguments = "+new_arguments)
+        else:
+            output_sub.write(wline)
+            # print(wline)
+
+    output_sub.close()
+    input_sub.close()
+
+if __name__ == "__main__":
+    convertSub("standard.sub", "my-pipeline")
