4bcb7f1e4966860e81615044fb9f2d932a737052
Gabriele <sucre.91@hotmail.it>
2020-01-09 17:08:07 +0100

    Minor fixes to main script

--- run-with-proxy-satellite.py
+++ run-with-proxy-satellite.py
@@ -8,7 +8,7 @@ satellite.setLogDir("./logs")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This is a process wrapper wich adds a proxyrearm satellite by default.')
-    parser.add_argument("main", type=str, help="Provide the main process launch command as a string (surrounded by quotes).")
+    parser.add_argument("main", type=str, required=True, help="Provide the main process launch command as a string (surrounded by quotes).")
     parser.add_argument("--name", "-n", type=str, help="Provide a custom name for the pipeline process wrapper.")
 
     args = parser.parse_args()
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     repetitionInterval = 12 * 60 * 60 - 400 # 12 hours minus 400 seconds to have time to renew the proxy...
     renewalThreshold = repetitionInterval/2 # doing it twice just for good measure
 
-    sideExes = [satellite.makeWrapper("proxyrearm/proxyrearm-oneclick.sh @ {}".format(repetitionInterval), mainExe.is_alive(), customName = "proxyrearm")]
+    sideExes = [satellite.makeWrapper("proxyrearm/proxyrearm-oneclick.sh @ {}".format(repetitionInterval), mainExe.is_alive, customName = "proxyrearm")]
 
     satellite.main(mainExe, sideExes)
