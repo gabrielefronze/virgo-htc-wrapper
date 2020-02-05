e2689464de6a1edbed32a978c38cc471e6436aac
gabrielefronze <sucre.91@hotmail.it>
2020-02-05 11:04:22 +0100

    Allowing custom CLI proxy renewal interval

--- run-with-proxy-satellite.py
+++ run-with-proxy-satellite.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This is a process wrapper wich adds a proxyrearm satellite by default.')
     parser.add_argument("main", type=str, help="Provide the main process launch command as a string (surrounded by quotes).")
     parser.add_argument("--name", "-n", type=str, help="Provide a custom name for the pipeline process wrapper.")
+    parser.add_argument("--interval", "-i", type=int, help="Provide the proxy renewal interval in seconds.")
 
     args = parser.parse_args()
 
@@ -19,8 +20,14 @@ if __name__ == "__main__":
     else:
         mainExe = satellite.makeWrapper(args.main, customName = args.name)
 
-    repetitionInterval = 12 * 60 * 60 - 400 # 12 hours minus 400 seconds to have time to renew the proxy...
-    renewalThreshold = repetitionInterval/2 # doing it twice just for good measure
+    renewalThreshold = 0
+
+    if not args.interval:
+        repetitionInterval = 12 * 60 * 60 - 400 # 12 hours minus 400 seconds to have time to renew the proxy...
+        renewalThreshold = repetitionInterval/2 # doing it twice just for good measure
+    else:
+        renewalThreshold = (int)args.interval
+
 
     fastlog(DEBUG, "Proxy renewal interval set to {}".format(renewalThreshold))
 
