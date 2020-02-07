f61f5e2c52c2e2dfe53d3ce2184d70efce9dce58
gabrielefronze <sucre.91@hotmail.it>
2020-02-07 14:49:15 +0100

    Improving install script

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
+if [[ ! -d "./virgo-htc-wrapper" ]]; then
+    git clone https://github.com/gabrielefronze/virgo-htc-wrapper.git
+fi
 
-git clone https://github.com/gabrielefronze/virgo-htc-wrapper.git
 cd virgo-htc-wrapper
 git pull
-git submodule init
-git submodule update
+
+if [[ ! -d "./virgo-htc-wrapper" ]]; then
+    git submodule init
+    git submodule update
+else
+    git submodule update --init --force
+fi
+
 cd -
